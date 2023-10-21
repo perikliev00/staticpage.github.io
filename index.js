39cf4eaf1dd0e8c6814bc50c61e6feb2e0c9f40d
Alexanmuco <117957025+perikliev00@users.noreply.github.com>
2023-10-21 17:00:42 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+let a=0;
+for(let i=0;i<200;i++) {
+    a++;
+    console.log(a);
+}
+let obj={
+    john:20 ,
+    alex:18 ,
+    petar:44
+}
+console.log(obj.john);
+obj.pesho=11;
+console.log(obj.pesho);
+let arr=[1,2,3,4,5];
+arr.push(33);
+arr.pop();
+console.log(arr.join(' '));
